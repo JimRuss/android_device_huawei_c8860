eeb75e4a616599f4594d70921e7d6cdf1f54da55
JimRuss <JimRuss.886@gmail.com>
2012-12-03 14:29:42 -0500

    replaced device .mk

--- cm.mk
+++ cm.mk
@@ -5,14 +5,14 @@ $(call inherit-product, vendor/cm/config/cdma.mk)
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/huawei/c8860/device_c8860.mk)
+$(call inherit-product, device/huawei/c8860/full_c8860.mk)
 
 # Select the right boot animation
 TARGET_BOOTANIMATION_NAME := vertical-480x854
 
 # Release name
 PRODUCT_RELEASE_NAME := HuaweiMercury
-PRODUCT_VERSION_DEVICE_SPECIFIC := C8860
+PRODUCT_VERSION_DEVICE_SPECIFIC := c8860
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/c8860/full_c8860.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, device/huawei/c8860/full_c8860.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := c8860
 PRODUCT_NAME := cm_c8860
-PRODUCT_BRAND := Huawei
+PRODUCT_BRAND := huawei
 PRODUCT_MODEL := C8860
 PRODUCT_MANUFACTURER := HUAWEI
-PRODUCT_CHARACTERISTICS := default
+PRODUCT_CHARACTERISTICS := phone
