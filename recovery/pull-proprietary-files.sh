94d2edb07c3de42e3c463469be7e299593a2df22
JimRuss <JimRuss.886@gmail.com>
2012-12-03 00:12:58 -0500

    removed shared object file

--- recovery/pull-proprietary-files.sh
+++ recovery/pull-proprietary-files.sh
@@ -4,7 +4,7 @@ PROPS=../../../../vendor/huawei/u8860/proprietary/recovery/
 
 echo "    -------------------- rmt_storage files"
 
-for f in libc.so libcutils.so libfotaagent.so liblog.so libm.so libstdc++.so libz.so linker offmode_charge offmode_check rmt_storage strace
+for f in libc.so libcutils.so liblog.so libm.so libstdc++.so libz.so linker offmode_charge offmode_check rmt_storage strace
         do cp $PROPS/$f .
 done
 
