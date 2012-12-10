90fe141754f1de2fc38438d2515c1522b2943292
JimRuss <JimRuss.886@gmail.com>
2012-12-10 15:06:08 -0500

    put .mk file back

--- device_c8860.mk
+++ device_c8860.mk
@@ -99,7 +99,8 @@ PRODUCT_COPY_FILES += \
 	device/huawei/c8860/configs/boot-c8860.rle:root/initlogo.rle \
 	device/huawei/c8860/configs/init.huawei.rc:root/init.huawei.rc \
 	device/huawei/c8860/configs/init.target.rc:root/init.target.rc \
-	device/huawei/c8860/configs/ueventd.huawei.rc:root/ueventd.huawei.rc 
+	device/huawei/c8860/configs/ueventd.huawei.rc:root/ueventd.huawei.rc \
+        device/huawei/c8860/configs/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh 
         
 # WLAN MODULES
 PRODUCT_COPY_FILES += \
