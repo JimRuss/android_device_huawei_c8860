fb407e29b21023b87b8d5545c11d0a7e005488b4
JimRuss <JimRuss.886@gmail.com>
2013-01-02 07:29:31 -0500

    add basic make file

--- prebuilt/modules/modules.mk
+++ prebuilt/modules/modules.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2010 The Android Open Source Project
+ # Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-
+ # Modules from precompiled kernel (until kernel is built from source)
  modules := \
        ansi_cprng.ko \
        cls_flow.ko \
@@ -45,4 +43,4 @@
  PRODUCT_COPY_FILES += $(foreach file,$(modules),\
          $(LOCAL_PATH)/$(file):system/lib/modules/$(file))
   
-       
+
