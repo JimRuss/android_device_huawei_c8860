d423602c4152644f4ebe049aebb692fdc8b51d62
JimRuss <JimRuss.886@gmail.com>
2013-01-05 19:41:18 -0500

    add rmt_storage

--- Android.mk
+++ Android.mk
@@ -3,4 +3,9 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),c8860)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 # include $(call all-named-subdir-makefiles, recovery)
+    subdir_makefiles := \
+        $(LOCAL_PATH)/recovery/rmt_storage/Android.mk \
+        $(LOCAL_PATH)/recovery/offmode/Android.mk 
+
+    include $(subdir_makefiles)
 endif
