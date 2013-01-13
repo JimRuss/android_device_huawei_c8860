3546e813e122c51f3c363284d0714432d10f18b7
JimRuss <JimRuss.886@gmail.com>
2013-01-13 14:49:19 -0500

    usb serial

--- device_c8860.mk
+++ device_c8860.mk
@@ -148,7 +148,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
         persist.sys.usb.config=mass_storage,adb \
         on property:sys.usb.config=mass_storage,adb \
-        on property:ro.serialno=cm-c8860
+        on property:ro.serialno=cm-c8860 \
+        persist.ro.serialno=cm-c8860
 
 # ADB access
 ADDITIONAL_DEFAULT_PROPERTIES += \
