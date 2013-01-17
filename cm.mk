d52d788cf38b7aa8f5af0c8c4b2d814174b1095e
JimRuss <JimRuss.886@gmail.com>
2013-01-17 13:29:44 -0500

    build fingerprint & id

--- cm.mk
+++ cm.mk
@@ -1,20 +1,17 @@
 # First, set the baseband technology
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := c8860
+
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/c8860/full_c8860.mk)
 
-# Select the right boot animation
+# Boot animation
 TARGET_BOOTANIMATION_NAME := vertical-480x854
-
-# Setup device specific product configuration.
-PRODUCT_NAME := cm_c8860
-
-# Release name
-PRODUCT_RELEASE_NAME := c8860
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := c8860
@@ -23,5 +20,12 @@ PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := c8860
 PRODUCT_MANUFACTURER := Huawei
 
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=c8860 \
+        BUILD_DISPLAY_ID="IMM76L" \
+        BUILD_FINGERPRINT=Huawei/c8860:4.0.4/IMM76L/124115:userdebug/test-keys \
+        PRIVATE_BUILD_DESC="Huawei-user 4.0.4 IMM76L 124115 test-keys" \
+        BUILD_NUMBER=124115
 
 
