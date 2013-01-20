f428c456968c606940d48ef96e7513a166068c79
JimRuss <JimRuss.886@gmail.com>
2013-01-20 00:33:57 -0500

    sync_sts

--- initscripts/init.qcom.sync.sh
+++ initscripts/init.qcom.sync.sh
@@ -31,4 +31,6 @@
 PATH=/system/bin:$PATH
 export PATH
 cat /sys/devices/platform/rs300000a7.65536/force_sync
+cat /sys/devices/platform/rs300100a7.65536/sync_sts
 cat /sys/devices/platform/rs300100a7.65536/force_sync
+cat /sys/devices/platform/rs300100a7.65536/sync_sts
