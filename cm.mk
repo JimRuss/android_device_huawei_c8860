c76a82d69db9515664978db0cd9513a5a694cc12
JimRuss <JimRuss.886@gmail.com>
2013-02-21 12:50:18 -0500

    edit LICENSE-2.0

--- cm.mk
+++ cm.mk
@@ -1,10 +1,25 @@
-# First, set the baseband technology
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := c8860
 
-# Inherit some common cyanogenmod stuff.(cm full installs main.conf)(switch to mini-phone in cm.mk will stop it)(QC's bt chip needs 1; but now BCM4329 needs 0)"DiscoverSchedulerInterval = 0"
+# Inherit some common cyanogenMod stuff.(cm full installs main.conf)(switch to mini-phone in cm.mk will stop it)(QC's bt chip needs 1; but now BCM4329 needs 0)"DiscoverSchedulerInterval = 0"
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
--- device_c8860.mk
+++ device_c8860.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Small language pack only
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -72,11 +88,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
         com.android.future.usb.accessory 
          
-# Wireless AP
-PRODUCT_PACKAGES += \
-        hostapd_cli \
-        hostapd
-
 # LED
 PRODUCT_PACKAGES += \
         Torch \
